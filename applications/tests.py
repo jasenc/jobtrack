1517732d6c81cdb75de4107ab7cbed0bb31021ca
jasenc <jasen.c8@gmail.com>
2015-10-21 16:55:36 -0700

    test: test_home_page_can_save_a_POST_request pass

--- applications/tests.py
+++ applications/tests.py
@@ -17,3 +17,12 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['application_text'] = 'A new application'
+
+        response = home_page(request)
+
+        self.assertIn('A new application', response.content.decode())

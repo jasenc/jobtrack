76c1628507493e9352b020f14a0d73d17648ace9
jasenc <jasen.c8@gmail.com>
2015-10-27 16:25:53 -0700

    refactor: views and tests

--- applications/views.py
+++ applications/views.py
@@ -4,34 +4,20 @@ from applications.models import Application
 
 # Create your views here.
 def home_page(request):
-    # If the request is POST,
-    if request.method == 'POST':
-        # create a new application object
-        Application.objects.create(
-            # while setting the company of the application equal to the value
-            # supplied by the POST form.
-            company=request.POST['application_company']
-        )
-        # Then redirect back to the home page.
-        return redirect('/applications/the-only-applications-in-the-world')
-
-    # Get all of the applications,
-    applications = Application.objects.all()
-    # Otherwise render a GET request of the home page and pass all of the
-    # applications along.
     return render(request, 'home.html')
 
 
 def view_applications(request):
     # Get all of the applications,
     applications = Application.objects.all()
-    # Otherwise render a GET request of the home page and pass all of the
-    # applications along.
+    # and pass them along to the applications template.
     return render(request, 'applications.html', {'applications': applications})
 
 
 def new_application(request):
+    # Get the company of the application supplied by the form's POST request.
     Application.objects.create(
         company=request.POST['application_company']
     )
+    # Redirect to a unique URL.
     return redirect('/applications/the-only-applications-in-the-world')

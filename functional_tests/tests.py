554ac0d3e4b776fca081440155a76636325c6d63
jasenc <jasen.c8@gmail.com>
2015-10-28 18:40:36 -0700

    tests: expand FT for layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -110,3 +110,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Facebook', page_text)
 
         # Satisfied, they both go back to sleep.
+
+    def test_layout_and_styling(self):
+        # Adeline goes to the home page.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered.
+        inputbox = self.browser.find_element_by_id('id_new_application')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and notices the input is nicely centered there
+        # as well.
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_tag_name('id_new_application')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )

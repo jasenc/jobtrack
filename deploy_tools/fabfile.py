91e9619945724a1e32cb2d2ed7880f06b03f0d0c
jasenc <jasen.c8@gmail.com>
2015-12-26 18:21:11 -0800

    docs: update virtualenv god damnit fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -49,7 +49,7 @@ def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/env/'
     if not exists(virtualenv_folder + '/bin/pip'):
         run('virtualenv --python=python3 %s' % (virtualenv_folder,))
-    run('%s/bin/pip install -r %srequirements.txt' % (virtualenv_folder,
+    run('%s/bin/pip install -r %s/requirements.txt' % (virtualenv_folder,
         source_folder))
 
 
